59f0147012cea44b4e9edc4526d9fbfc6b011824
Jessica <jsisavath@gmail.com>
2022-07-24 19:07:34 -0500

    started directory

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import About from './components/About/homepage';
+
+function App() {
+
+  return (
+    <div>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
