b02eb472735a507ba40ed3ef268c8d3244ba2f70
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-07-25 20:12:22 -0500

    created base components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import About from './components/About/homepage';
 import LoginForm from './components/Login';
-import HomepageLayout from './components/Homepage';
+
 
 
 function App() {
@@ -9,9 +8,7 @@ function App() {
   return (
     <div>
       <main>
-        <About></About>
         <LoginForm></LoginForm>
-        <HomepageLayout></HomepageLayout>
       </main>
     </div>
   );
