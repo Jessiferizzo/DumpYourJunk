ed8a85291b399bfa4e6b4e3db65667bae966c354
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-07-25 21:26:33 -0500

    created login page style

---
+++ src/components/Login/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Button, Form, Grid, Header, Message, Segment, Icon } from 'semantic-ui-react'
+
+const LoginForm = () => (
+  <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+    <Grid.Column style={{ maxWidth: 500 }}>
+
+      <Header as='h2' color='blue' textAlign='Center'>
+        <Icon name='sign in' size='big'></Icon>Login to your account</Header>
+      <Segment >
+        <Form>
+          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+          <Form.Input
+            fluid
+            icon='lock'
+            iconPosition='left'
+            placeholder='Password'
+            type='password'
+          />
+          <Button type='submit' fluid size='Large' color='blue'>Submit</Button>
+        </Form>
+      </Segment>
+      <Message>
+        {/*Link below need to link to our sign-up page */}
+        Need to {/*<a href='#'> Sign-up?</a>*/}
+      </Message>
+    </Grid.Column>
+  </Grid>
+)
+
+export default LoginForm
