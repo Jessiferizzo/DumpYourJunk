46e6a64f4a271cab46b9eb300aeccdc98682bb8d
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-07-26 19:05:42 -0500

    saving develop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Album from './components/Gallery';
 import LoginForm from './components/Login';
+<<<<<<< HEAD
 import Nav from './components/Nav'
 import HomePage from './components/Homepage';
 import SignUp from './components/Singup';
 
+=======
+import About from './components/About';
+import Checkout from './components/Checkout/Checkout';
+import PrimarySearchAppBar from './components/Nav';
+>>>>>>> 654c03f82401dac556b239ebc60976adb2708cec
 
 
 function App() {
@@ -11,11 +18,22 @@ function App() {
   return (
     <div>
       <main>
+<<<<<<< HEAD
         <Nav></Nav>
         <HomePage></HomePage>
+=======
+      <PrimarySearchAppBar></PrimarySearchAppBar>
+        <About></About>
+>>>>>>> 654c03f82401dac556b239ebc60976adb2708cec
         <LoginForm></LoginForm>
         <SignUp></SignUp>
       </main>
+      <section>
+        <Album></Album>
+      </section>
+      <section>
+        <Checkout></Checkout>
+      </section>
     </div>
   );
 }
