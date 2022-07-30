46fc9c64762baf89a8a7174051bfab5a8860ac4e
Blessing <bigbadumhe@gmail.com>
2022-07-30 00:32:18 -0500

    bless client

---
+++ blessclient/src/components/ProductList/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ProductList = ({ products, title }) => {
+  if (!products.length) {
+    return <h3> There is no product to display </h3>;
+  }
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      {products &&
+        products.map((product) => (
+          <div key={product._id} className="card mb-3">
+            <p className="card-header">
+              {product.username}
+              <br />
+              Created on {product.createdAt}
+            </p>
+            <div className="card-body">
+              <p>{product.productname}</p>
+              <img src={product.image} alt="prodImg"/>
+              {/* <p>{product.image}</p> */}
+              <p>{product.description}</p>
+              <p>{product.category}</p>
+              <p>${product.price}</p>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default ProductList;
---
+++ blessclient/src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_PRODUCTS } from "../utils/queries";
+import ProductList from "../components/ProductList";
+
+const Home = () => {
+  // use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  const products = data?.products || [];
+  console.log(products);
+
+  return (
+    <main>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">{loading ? (<div>Loading</div>):(<ProductList products={products} title="Buy Some Beautiful Junks" />)}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
