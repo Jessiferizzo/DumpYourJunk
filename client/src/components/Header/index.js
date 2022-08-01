88c9769983db5fdc4dc79b692991d9c6a7a3d24b
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-08-01 00:03:12 -0500

    styled the header and search bar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,13 +1,63 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
-import { Avatar, IconButton } from '@mui/material';
+import { Avatar, Box, IconButton } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import Typography from '@mui/material/Typography';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2,),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '70%',
+  [theme.breakpoints.down('lg')]: {
+    marginLeft: theme.spacing(3),
+    width: '40%',
+  },
+  [theme.breakpoints.up('lg')]: {
+    marginLeft: theme.spacing(3),
+    width: '40%',
+  },
+  [theme.breakpoints.down('md')]: {
+    marginLeft: theme.spacing(3),
+    width: '70%',
+  },
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '100%',
+    },
+  },
+}));
 
 const Header = ({ searchValue, setSearchValue, cartProducts }) => {
   const logout = (event) => {
@@ -17,6 +67,7 @@ const Header = ({ searchValue, setSearchValue, cartProducts }) => {
 
   return (
     <AppBar position="Relative" >
+      {/* className=" justify-space-between-lg  "*/}
       <Toolbar className=" justify-space-between-lg  ">
         <Link to="/">
           <Avatar sx={{ m: 2, bgcolor: "#fff" }}>
@@ -28,8 +79,30 @@ const Header = ({ searchValue, setSearchValue, cartProducts }) => {
         <Typography variant="h6" color="inherit">
           Sell your Junk
         </Typography>
-        <input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} className="search-input" placeholder="filter" />
+        {Auth.loggedIn() ? (
+            <>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Search>
+                  <SearchIconWrapper>
+                    <SearchIcon />
+                  </SearchIconWrapper>
+                  <StyledInputBase
+                    type="text"
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    className="search-input"
+                    placeholder="Search…"
+                    inputProps={{ 'aria-label': 'search' }}
+                  />
+                </Search>
+              </Typography>
 
+            </>
+          ) : (
+            <>
+            
+            </>
+          )}
         <nav className="text-center nav-links">
           {Auth.loggedIn() ? (
             <>
@@ -37,7 +110,10 @@ const Header = ({ searchValue, setSearchValue, cartProducts }) => {
               <a href="/" onClick={logout}>
                 Logout
               </a>
-
+              <Box className="cart">
+                <span className="cart-no">{cartProducts.length}</span>
+                <ShoppingCartIcon ></ShoppingCartIcon>
+              </Box>
             </>
           ) : (
             <>
@@ -46,10 +122,7 @@ const Header = ({ searchValue, setSearchValue, cartProducts }) => {
             </>
           )}
         </nav>
-        <div className="cart">
-          <span className="cart-no">{cartProducts.length}</span>
-          <FontAwesomeIcon icon={faCartShopping} />
-        </div>
+
       </Toolbar>
     </AppBar>
   );
