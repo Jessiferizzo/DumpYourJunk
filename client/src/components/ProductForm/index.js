70abb016169a0b1e56a39b5ad3705bdc3535bba6
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-08-03 23:57:16 -0500

    commit

--- client/src/components/ProductForm/index.js
+++ client/src/components/ProductForm/index.js
@@ -95,45 +95,44 @@ const handleClose = () => setOpen(false);
         className="flex-row justify-center justify-space-between-md align-stretch"
         onSubmit={handleFormSubmit}
       >
-        <input
+        <TextField
           placeholder="Here's a new Product name"
           name="productname"
           value={productState.productname}
           className="form-input col-12 col-md-9"
           onChange={handleChange}
         />
-        <input
+        <TextField
           placeholder="Description"
           name="description"
           value={productState.description}
           className="form-input col-12 col-md-9"
           onChange={handleChange}
         />
-        <input
+        <TextField
           placeholder="Category"
           name="category"
           value={productState.category}
           className="form-input col-12 col-md-9"
           onChange={handleChange}
         />
-        <input
+        <TextField
           placeholder="image"
           name="image"
           value={productState.image}
           className="form-input col-12 col-md-9"
           onChange={handleChange}
         />
-        <input
+        <TextField
           placeholder="price"
           name="price"
           value={productState.price}
           className="form-input col-12 col-md-9"
           onChange={handleChange}
         />
-        <br />
-        <button className="btn col-12 col-md-3" type="submit">
+        <Button variant="contained" className="btn col-12 col-md-3" type="submit">
           Submit
-        </button>
+        </Button>
       </form>
     </div>
   );
